2e64a0a5f6973821d00a8e95e9c0c5ec0ec9ceaa
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2022-12-16 22:17:47 -0400

    code example

--- hospital/models.py
+++ hospital/models.py
@@ -11,7 +11,20 @@ class Patient(models.Model):
 class Medic(models.Model):
     name = models.CharField(max_length=50)
     room = models.CharField(max_length=2)
+    patients = models.ManyToManyField(
+        Patient, through="Schedule", related_name="medics"
+    )
 
     def __str__(self) -> str:
         return f"{self.name} | {self.room}"
 
+class Schedule(models.Model):
+    medic = models.ForeignKey(
+        "Medic",
+        on_delete=models.CASCADE,
+    )
+    patient = models.ForeignKey(
+        "Patient",
+        on_delete=models.CASCADE
+    )
+    description = models.TextField()
